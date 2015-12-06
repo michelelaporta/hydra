67051a7e865733f7ea810e395e9b1ade938db809
mlaporta <mlaporta@tykelab.it>
2015-12-06 01:21:46 +0100

    cleanup

--- public/javascripts/preferences.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-//	$('input[id=email]').val('mmmm@aaaa.it');
-//	var r = $('input[id=email]').val();
-//	console.log('r ' + r);
-});
-
-
--- routes/about.js
+++
@@ -1,5 +0,0 @@
-
-exports.list = function(req, res){
-  //res.send("respond with a resource");
-  res.render('about', { title: 'About Hydra', what: 'hydroponics green house management software'})
-};
